3fdfecb0a8bafab12bcd8e43c048990201f3ae5a
Friedhelm Filler <fried@ffiller.com>
2021-04-06 11:31:54 +0200

    bring in Josh's company names

--- lib/ffaker/company.rb
+++ lib/ffaker/company.rb
@@ -10,11 +10,7 @@ module FFaker
     POSITIONS = %w[President Manager Director Secretary Consultant].freeze
 
     def name
-      case rand(0..2)
-      when 0 then "#{Name.last_name} #{suffix}"
-      when 1 then "#{Name.last_name}-#{Name.last_name}"
-      when 2 then format('%s, %s and %s', Name.last_name, Name.last_name, Name.last_name)
-      end
+      ['Acme, Inc.', 'Widget Corp', 'Smith and Co.', 'Foo Bars', 'ABC Telecom', 'QWERTY Logistics', 'Acme Corp', 'Allied Biscuit', 'Extensive Enterprise', 'Galaxy Corp', 'Globo-Chem', 'Mr. Sparkle', 'Globex Corporation', 'LexCorp', 'LuthorCorp', 'North Central Positronics', 'Omni Consimer Products', 'Praxis Corporation', 'Sombra Corporation', 'Wayne Enterprises', 'Wentworth Industries', 'ZiffCorp', 'Bluth Company', 'Strickland Propane', 'Thatherton Fuels', 'Three Waters', 'Water and Power', 'Western Gas & Electric', 'Mammoth Pictures', 'Gringotts', 'Thrift Bank', 'Osato Chemicals', 'Transworld Consortium', 'Universal Export', 'Virtucon', 'Kumatsu Motors', 'Keedsler Motors', 'Powell Motors', 'Industrial Automation', 'Sirius Cybernetics Corporation', 'U.S. Robotics and Mechanical Men', 'Colonial Movers', 'Corellian Engineering Corporation', 'Incom Corporation', 'General Products', 'Leeding Engines Ltd.', 'Blammo', 'Input, Inc.', 'Mainway Toys', 'Videlectrix', 'Zevo Toys', 'Axis Chemical Co.', 'Barrytron', 'Carrys Candles', 'Cogswell Cogs', 'Spacely Sprockets', 'General Forge and Foundry', 'Duff Brewing Company', 'Dunder Mifflin', 'General Services Corporation', 'Monarch Playing Card Co.', 'Krustyco', 'Initech', 'Roboto Industries', 'Primatech', 'Vandelay Industries', 'Wernham Hogg', 'Gadgetron', 'Burleigh and Stronginthearm', 'BLAND Corporation', 'Nordyne Defense Dynamics', 'Petrox Oil Company', 'Roxxon', 'McMahon and Tate', 'Spade and Archer', 'Rouster and Sideways', 'C.H. Lavatory and Sons', 'Globo Gym American Corp', 'The New Firm', 'SpringShield', 'Compuglobalhypermeganet', 'Data Systems', 'Gizmonic Institute', 'Initrode', 'Taggart Transcontinental', 'Atlantic Northern'].sample
     end
 
     def suffix
